e6e34cdd5a03ec0d82562369a631d4abcfc94fdd
Zach D'Amico <zachdamico90@gmail.com>
2023-11-17 10:27:27 -0600

    updated publication date

--- rareapi/models/posts.py
+++ rareapi/models/posts.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     rare_user = models.ForeignKey('RareUser', on_delete=models.CASCADE, related_name="posts")
     category = models.ForeignKey('Categories', on_delete=models.CASCADE, related_name="post_category")
     title = models.CharField(max_length=250)
-    publication_date = models.DateField()
+    publication_date = models.DateField(auto_now_add=True)
     image_url = models.URLField()
     content = models.TextField()
     approved = models.BooleanField(default=False)
