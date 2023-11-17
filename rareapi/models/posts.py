43cb4dbacf26818b5407688be2c80b1d6da26317
Zach D'Amico <zachdamico90@gmail.com>
2023-11-17 10:18:33 -0600

    post model and fixture complete

---
+++ rareapi/models/posts.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    rare_user = models.ForeignKey('RareUser', on_delete=models.CASCADE, related_name="posts")
+    category = models.ForeignKey('Categories', on_delete=models.CASCADE, related_name="post_category")
+    title = models.CharField(max_length=250)
+    publication_date = models.DateField()
+    image_url = models.URLField()
+    content = models.TextField()
+    approved = models.BooleanField(default=False)
