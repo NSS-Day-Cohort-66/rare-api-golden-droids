3a819c5d6ff0af97a85cf959f271d1c01fdce024
Zach D'Amico <zachdamico90@gmail.com>
2023-11-17 10:48:57 -0600

    updated category related name

--- rareapi/models/__init__.py
+++ rareapi/models/__init__.py
@@ -1,1 +1,2 @@
+
 from .posts import Post
--- rareapi/models/posts.py
+++ rareapi/models/posts.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     rare_user = models.ForeignKey('RareUser', on_delete=models.CASCADE, related_name="posts")
-    category = models.ForeignKey('Categories', on_delete=models.CASCADE, related_name="post_category")
+    category = models.ForeignKey('Categories', on_delete=models.CASCADE, related_name="posts")
     title = models.CharField(max_length=250)
     publication_date = models.DateField(auto_now_add=True)
     image_url = models.URLField()
