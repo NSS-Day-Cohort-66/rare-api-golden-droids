c29ca563c4b036ff5d6e69ee35a2dc36e24e0f31
Tricia Swift <tricia.swift8@gmail.com>
2023-11-20 11:52:32 -0600

    wip

--- rare/urls.py
+++ rare/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from rareapi.views import login_user
+from rest_framework import routers
+from rareapi.views import CommentView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r"comments", CommentView, "comment")
 
 urlpatterns = [
+    path("", include(router.urls)),
     path('admin/', admin.site.urls),
     path('login', login_user),
 ]
--- rareapi/views/__init__.py
+++ rareapi/views/__init__.py
@@ -1,1 +1,2 @@
 from .auth import login_user
+from .comments import CommentView
