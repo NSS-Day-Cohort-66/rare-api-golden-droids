6aca4dbf8e9a322e1e893fa19ca97df7fe6c9d02
Sylvia T <sylviarosemarie@gmail.com>
2023-11-20 14:40:47 -0600

    Stashing - In process of working thru registration

--- rare/urls.py
+++ rare/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from rareapi.views import login_user
+from rest_framework import routers
+
+router = routers.DefaultRouter(trailing_slash=False)
+
 
 urlpatterns = [
+    path("", include(router.urls)),
     path('admin/', admin.site.urls),
     path('login', login_user),
+    path('register', login_user),
 ]
--- rareapi/models/__init__.py
+++ rareapi/models/__init__.py
@@ -4,3 +4,4 @@ from .category import Category
 from .post import Post
 from .tag import Tag
 from .rareuser import RareUser
+
