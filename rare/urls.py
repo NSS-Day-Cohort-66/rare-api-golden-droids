9597c7979c75099023e25d3020fe491f8e441bc8
Zach D'Amico <zachdamico90@gmail.com>
2023-11-20 13:52:54 -0600

    still working on post client components

--- rare/urls.py
+++ rare/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from rareapi.views import login_user
+from django.urls import path, include
+from rareapi.views import login_user, UserPostView
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r'posts', UserPostView, 'post')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('login', login_user),
 ]
--- rareapi/views/__init__.py
+++ rareapi/views/__init__.py
@@ -1,1 +1,2 @@
 from .auth import login_user
+from .posts import UserPostView
