c8e7827c0f60ab735bea7685d8d92088a2ac9fdc
Zach D'Amico <zachdamico90@gmail.com>
2023-11-21 14:01:30 -0600

    create new tag ticket complete

--- rareapi/views/tags.py
+++ rareapi/views/tags.py
@@ -12,6 +12,15 @@ class TagView(ViewSet):
         tags = Tag.objects.all().order_by('label')
         serializer = TagSerializer(tags, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def create(self, request):
+        label = request.data.get('label')
+
+        tag = Tag.objects.create(
+            label=label
+        )
+        serializer = TagSerializer(tag, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class TagSerializer(ModelSerializer):
     class Meta:
