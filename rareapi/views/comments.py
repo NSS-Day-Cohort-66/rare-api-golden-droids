f13acc1d465b2500994e30dc52b753df11d3734b
Tricia Swift <tricia.swift8@gmail.com>
2023-11-27 09:13:10 -0600

    wip

--- rareapi/views/comments.py
+++ rareapi/views/comments.py
@@ -5,10 +5,31 @@ from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from rest_framework import status
 from rareapi.models import Post, Comment, RareUser
 from django.contrib.auth.models import User
+from django.http import HttpResponseServerError
 
 
 class CommentView(ViewSet):
     """Rare comments view"""
+
+    def list(self, request):
+        """Handle GET requests for all comments
+
+        Returns:
+            Response -- JSON serialized array
+        """
+        post_id = self.request.query_params.get("post", None)
+
+        try:
+            comments = Comment.objects.all().order_by('created_on')
+            # Format created_on to only have the date before serializing
+            if post_id is not None:
+                comments = Comment.objects.filter(post_id=post_id).order_by('created_on')
+            for comment in comments:
+                comment.created_on = comment.created_on.strftime("%m-%d-%Y")
+            serializer = CommentSerializer(comments, many=True, context={"request": request})
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
 
     def create(self, request):
         """Handle POST operations
