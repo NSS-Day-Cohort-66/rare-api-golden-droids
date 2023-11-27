a234d1b3a3be3453d2fe772abbf987514ba843e9
Tricia Swift <tricia.swift8@gmail.com>
2023-11-27 16:56:01 -0600

    wip

--- rareapi/views/rareusers.py
+++ rareapi/views/rareusers.py
@@ -1,9 +1,5 @@
-from rest_framework import viewsets, status, permissions
-from rest_framework.decorators import action
-from rest_framework.response import Response
+from rest_framework import viewsets
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token
-from django.contrib.auth.models import User
 from rareapi.models import RareUser
 
 
@@ -13,16 +9,4 @@ class RareUserSerializer(serializers.ModelSerializer):
         fields = ('id', 'bio', 'profile_image_url', 'created_on', 'active', 'user_id')  # add other fields as needed
         
 class RareUserViewSet(viewsets.ViewSet):
-    queryset = RareUser.objects.all()
-    permission_classes = [permissions.AllowAny]
-
-    @action(detail=False, methods=['post'], url_path='register')
-    def register_rare_account(self, request):
-        serializer = RareUserSerializer(data=request.data)
-        if serializer.is_valid():
-            rareuser = User.objects.create_user(
-                bio=serializer.validated_data['bio']
-            )
-            token, created = Token.objects.get_or_create(rareuser=rareuser)
-            return Response({"Valid": "true", "Created": created}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)        
+    pass      
