01184fc101765a04328a3e90b9637ea4ab2f7a41
PhiferTristan <phifer.tristan@outlook.com>
2023-12-01 10:11:59 -0600

    commit to switch branch

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,3 +2,4 @@ from .categories_test import CategoryTest
 from .tags_test import TagTest
 from .users_test import UserTest
 from .posts_test import PostTest
+from .post_tags_test import PostTagTest
--- tests/post_tags_test.py
+++ tests/post_tags_test.py
@@ -8,3 +8,22 @@ class PostTagTest(APITestCase):
     fixtures = ['tags', 'users', 'posts', 'tokens']
 
     def setUp(self):
+        self.user = User.objects.first()
+        self.user.is_staff = True
+        self.user.save()
+        token = Token.objects.get(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {token.key}")
+
+    def test_get_all_tags(self):
+        response = self.client.get(f"/post_tags")
+
+        json_response = json.loads(response.content)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(json_response[0]["id"], "1")
+        self.assertEqual(json_response[0]["post_id"], "1")
+        self.assertEqual(json_response[0]["tag_id"], "2")
+        self.assertEqual(json_response[1]["id"], "2")
+        self.assertEqual(json_response[1]["post_id"], "2")
+        self.assertEqual(json_response[1]["tag_id"], "4")
